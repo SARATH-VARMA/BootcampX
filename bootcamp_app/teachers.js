7c75fba6e88b69a9a90df887f106d8d2d474fe15
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-07-31 19:33:02 -0400

    Parameterized Query-teachers

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,4 @@
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -6,15 +7,19 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+  `;
+const cohortName = process.argv[2] || 'JUL02';
+// Store all potentially malicious values in an array. 
+const values = [cohortName];
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
